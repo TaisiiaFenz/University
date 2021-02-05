11bcf99440bd16789a69240b3da77fe53e62825c
TaisiiaFenz <tayafenz@gmail.com>
2021-02-05 14:02:00 +0200

    add liquibase

---
+++ Semester_6/OOP/Lab_1/src/main/java/ua/taisiia/config/Main.java
@@ -0,0 +1,9 @@
+package ua.taisiia.config;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBaseConnection test = new DataBaseConnection();
+        test.initializeDataBase();
+    }
+
+}
