5d235fd47c222893518bee7c0a22fd589df7ccda
TaisiiaFenz <tayafenz@gmail.com>
2021-02-06 14:56:52 +0200

    add command structure

--- Semester_6/OOP/Lab_1/src/main/java/ua/taisiia/controller/AppServlet.java
+++ Semester_6/OOP/Lab_1/src/main/java/ua/taisiia/controller/AppServlet.java
@@ -1,5 +1,7 @@
 package ua.taisiia.controller;
 
+import ua.taisiia.controller.command.Command;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/add-description"})
 public class AppServlet extends HttpServlet {
---
+++ Semester_6/OOP/Lab_1/src/main/java/ua/taisiia/controller/command/MultipleMethodCommand.java
@@ -0,0 +1,16 @@
+package ua.taisiia.controller.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public abstract class MultipleMethodCommand implements Command{
+    @Override
+    public String execute(HttpServletRequest request) throws Exception {
+        String type = request.getMethod();
+
+        return "GET".equals(type) ? executeGet(request) : executePost(request);
+    }
+
+    protected abstract String executeGet(HttpServletRequest request);
+
+    protected abstract String executePost(HttpServletRequest request) throws Exception;
+}
