d8f3454a83a320ed4288bde9ea1ba4d47a1ccc24
TaisiiaFenz <tayafenz@gmail.com>
2021-04-24 14:32:33 +0300

    add models

---
+++ Semester_6/DistributedComputing/Module_2/Lab_2_6/src/main/java/models/City.java
@@ -0,0 +1,40 @@
+package models;
+
+public class City {
+    private int cityId;
+    private String cityName;
+    private int populationInThousands;
+    private int countryId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getPopulationInThousands() {
+        return populationInThousands;
+    }
+
+    public void setPopulationInThousands(int populationInThousands) {
+        this.populationInThousands = populationInThousands;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+}
---
+++ Semester_6/DistributedComputing/Module_2/Lab_2_6/src/main/java/models/Country.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Country {
+    private int countryId;
+    private String countryName;
+    private String capital;
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+}
